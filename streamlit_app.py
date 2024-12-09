29a3d0f84ee45437e8d8d4ae82c509a03caf0425
cguerreroto <55792679+cguerreroto@users.noreply.github.com>
2024-12-09 21:00:02 +0000

    Testing the file upload component

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("Let's figure out where it saves an uploaded file.")
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes = uploaded_file.getvalue()
+    st.write(f"Uploaded a file with {len(bytes)} length.")
